765b06302b13170a674d22b9888a8d7e2860e7e9
Adrianne M <affirmwealth@gmail.com>
2014-07-01 02:40:38 -0500

    Linked up student with assignments.

---
+++ app/models/assignments.rb
@@ -0,0 +1,3 @@
+class Assignments < ActiveRecord::Base
+  belongs_to :student
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701050039) do
+ActiveRecord::Schema.define(version: 20140701073554) do
+
+  create_table "assignments", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "students", force: true do |t|
     t.string   "title"
